62ff3552ea8295a0e19ee7856e5399038e1ae920
MaryKroustali <mariakroust.mk.mk@gmail.com>
2025-04-19 12:52:58 +0300

    trblsht

--- scripts/buildagent.sh
+++ scripts/buildagent.sh
@@ -9,8 +9,8 @@ org="MaryKroustali"  # Replace with your GitHub organization name
 repo="containerized_app_on_azure"  # Replace with your GitHub repository name
 
 # Install helping tools
-sudo apt-get update -y
-sudo apt-get install jq -y
+# sudo apt-get update -y
+# sudo apt-get install jq -y
 
 # Set Github Runner
 cd /home/vmadmin/ # Create a folder under admin directory
@@ -23,26 +23,28 @@ response=$(curl -s -L \
   -H "Authorization: Bearer $token" \
   -H "X-GitHub-Api-Version: 2022-11-28")
 registrationToken=$(echo "$response" | jq -r .token)
+echo $registrationToken
 sudo chown -R vmadmin .
 sudo chmod -R 755 .
-./config.sh --unattended --url https://github.com/$org/$repo/ --token $registrationToken --replace # Create the runner
-sudo ./svc.sh install
-sudo ./svc.sh start # Start running as a service
+echo "configuring........................."
+echo | ./config.sh --unattended --url https://github.com/$org/$repo/ --token $registrationToken --replace # Create the runner
+./svc.sh install
+./svc.sh start # Start running as a service
 
-# Install az cli
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-az --version
+# # Install az cli
+# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+# az --version
 
-# Install Docker
-sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-docker --version
+# # Install Docker
+# sudo apt-get update -y
+# sudo apt-get install -y ca-certificates curl gnupg lsb-release
+# sudo install -m 0755 -d /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+# sudo chmod a+r /etc/apt/keyrings/docker.asc
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update -y
+# sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# docker --version
